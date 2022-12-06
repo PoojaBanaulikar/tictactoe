9c7729926a8b146d037ff37418d72a7d9ce4c4a9
PoojaBanaulikar <poojabanaulikar01@gmail.com>
2022-12-06 22:09:49 +0530

    added statusMessage component

---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StatusMessage = () => {
+
+    // const message = winner 
+    // ? `Winner is ${winner}`
+    //  : `Next player is ${current.isXNext ? 'X' :'O'}`;
+  
+  return (
+    <h2>dd</h2>
+  )
+}
+
+export default StatusMessage
